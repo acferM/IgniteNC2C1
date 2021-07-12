5e3ae836345cdf1509314b630b210679952d913e
acferM <matheus.acfer@nave.org.br>
2021-07-12 19:47:04 -0300

    All tests passing

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,44 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const find = this.users.find((user) => user.id === id);
+
+    return find;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const find = this.users.find((user) => user.email === email);
+
+    return find;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const findIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+
+    this.users[findIndex] = { ...this.users[findIndex], admin: true };
+
+    return this.users[findIndex];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
